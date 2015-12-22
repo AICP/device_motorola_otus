3cd3e009db0281a87bf590e3f5d5fe9f8f06eb31
Trafalgar-Square <schneller.demian@gmail.com>
2015-12-22 15:02:30 +0100

    Move otus to AICP.
    Change-Id: I9ff3bf2f7706ba0f26fd509f5233fdb0ec704454

---
+++ device.mk
@@ -0,0 +1,54 @@
+#
+# Copyright (C) 2015 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+# This file includes all definitions that apply to ALL otus devices, and
+# are also specific to otus devices
+#
+# Everything in this directory will become public
+
+LOCAL_PATH := device/motorola/otus
+
+# device specific vendor blobs
+$(call inherit-product-if-exists, vendor/motorola/otus/otus-vendor.mk)
+
+# Ramdisk
+ PRODUCT_COPY_FILES += \
+     $(call find-copy-subdir-files,*,${LOCAL_PATH}/ramdisk,root)
+
+# Prebuilt
+PRODUCT_COPY_FILES += \
+     $(call find-copy-subdir-files,*,${LOCAL_PATH}/prebuilt/system,system)
+
+# Audio configuration
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/vendor/etc/audio_effects.conf \
+    $(LOCAL_PATH)/audio/audio_policy.conf:system/etc/audio_policy.conf \
+    $(LOCAL_PATH)/audio/mixer_paths.xml:system/etc/mixer_paths.xml
+
+DEVICE_PACKAGE_OVERLAYS := \
+    $(LOCAL_PATH)/overlay
+
+# Support FRP features (enable bootloader unlock etc.)
+PRODUCT_PROPERTY_OVERRIDES += \
+    ro.frp.pst=/dev/block/platform/msm_sdcc.1/by-name/frp
+
+# Prima(pronto firmware)
+PRODUCT_COPY_FILES += \
+    kernel/motorola/otus/drivers/staging/prima/firmware_bin/WCNSS_cfg.dat:system/etc/firmware/wlan/prima/WCNSS_cfg.dat \
+    kernel/motorola/otus/drivers/staging/prima/firmware_bin/WCNSS_qcom_cfg.ini:system/etc/firmware/wlan/prima/WCNSS_qcom_cfg.ini
+
+# Inherit from msm8610-common
+$(call inherit-product, device/motorola/msm8610-common/msm8610.mk)
