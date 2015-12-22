ac0565d961c226d810e92b3b5599eee8d8ee4f1e
Trafalgar-Square <schneller.demian@gmail.com>
2015-12-22 15:13:58 +0100

    Move to AICP
    Change-Id: I6896c7fa40716483776f1d2daea77927020e7b04

--- cm.mk
+++ cm.mk
@@ -1,7 +1,7 @@
 $(call inherit-product, device/motorola/otus/full_otus.mk)
 
 # Inherit some common CM stuff.
-$(call inherit-product, vendor/cm/config/common_full_phone.mk)
+$(call inherit-product, vendor/aicp/configs/common_full_phone.mk)
 
 ## Device identifier. This must come after all inclusions
 PRODUCT_NAME := cm_otus
